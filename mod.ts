ff4abe349fee3f69ffc42daa80e5072d8c47d640
Tim Kye <tkye@squareup.com>
2023-06-16 13:26:52 -0700

    feat: add cache-control blocks for index

--- mod.ts
+++ mod.ts
@@ -2,12 +2,25 @@ import { serve } from 'https://deno.land/std@0.190.0/http/server.ts'
 import { serveDir } from 'https://deno.land/std@0.190.0/http/file_server.ts'
 
 serve(async (req) => {
+  const url = new URL(req.url)
+  let isIndex = url.pathname === '/' || url.pathname === '/index.html'
+
   let res = await serveDir(req)
 
   if (res.status == 404) {
     const index = new URL(req.url)
     index.pathname = 'index.html'
+    isIndex = true
     res = await serveDir(new Request(index, req))
+  }
+
+  if (isIndex) {
+    res.headers.set(
+      'cache-control',
+      'max-age=0, no-cache, no-store, must-revalidate'
+    )
+    res.headers.set('pragma', 'no-cache')
+    res.headers.set('expires', '0')
   }
 
   return res
